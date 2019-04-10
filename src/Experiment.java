1621b698c9f20d4496210d2696bcdb7dcd10dd42
Vojtech Horky <vojtech.horky@gmail.com>
2019-04-10 10:05:48 +0200

    Add sources

---
+++ src/Experiment.java
@@ -0,0 +1,28 @@
+import java.util.Map;
+
+public class Experiment {
+    private static final String[] INTERESTING_PREFIXES = new String[] {
+        "java.vm.",
+        "java.vendor.",
+        "java.version",
+    };
+    public static void main(String args[]) {
+        if (args.length == 0) {
+            printAll();
+        } else {
+            System.out.println(System.getProperty(args[0], ""));
+        }
+    }
+    
+    private static void printAll() {
+        for (Map.Entry<?, ?> entry : System.getProperties().entrySet()) {
+            String name = entry.getKey().toString();
+            for (String prefix : INTERESTING_PREFIXES) {
+                if (name.startsWith(prefix)) {
+                    String value = entry.getValue().toString();
+                    System.out.printf("%s = %s\n", name, value);
+                }
+            }
+        }
+    }
+}

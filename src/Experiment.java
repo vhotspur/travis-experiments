7116b3f5d44737ce67599de2d337d93db0db5815
Vojtech Horky <vojtech.horky@gmail.com>
2019-04-29 07:37:29 +0200

    Add version printing

--- src/Experiment.java
+++ src/Experiment.java
@@ -1,14 +1,22 @@
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Map;
 
 public class Experiment {
+    private static final String VERSION = "0.1";
+
     private static final String[] INTERESTING_PREFIXES = new String[] {
         "java.vm.",
         "java.vendor.",
         "java.version",
     };
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException {
         if (args.length == 0) {
             printAll();
+        } else if (args[0].equals("--update-version")) {
+            PrintStream ver = new PrintStream("version.txt");
+            ver.println(VERSION);
+            ver.close();
         } else if (args[0].equals("--list")) {
             System.out.println("java.vm.name");
             System.out.println("java.vm.vendor");
